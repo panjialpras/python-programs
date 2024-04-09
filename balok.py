4a47de68bc9b895401ddf2692a54e791e6617f4c
panjialpras <kiminari@supermail.id>
2024-04-09 12:01:35 +0700

    Input, aritmethic

---
+++ balok.py
@@ -0,0 +1,11 @@
+# Menghitung volume dan luas sebuah balok
+
+lebar = float(input("lebar: "))
+panjang = float(input("panjang: "))
+tinggi = float(input("tinggi: "))
+
+volume = lebar * panjang * tinggi
+luas = lebar * panjang
+
+print(volume)
+print(luas)
