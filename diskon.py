3a46a5df4fae6d057f8c53dfa8acb021719c5b08
panjialpras <kiminari@supermail.id>
2024-04-09 12:09:33 +0700

    Math library and simple calculation

--- diskon.py
+++ diskon.py
@@ -1,14 +1,17 @@
-print ("----- Menghitung Diskon Sebuah Barang -----")
+import math
+
+print("----- Menghitung Diskon Sebuah Barang -----")
 
 a = int(input("masukkan harga yang ingin ditentukan: "))
 
-if (a >= 500000) :
-    harga = a * 0.2
-elif (a >= 300000) :
-    harga = a * 0.1
-elif (a >= 150000) :
-    harga = a * 0.05
+if (a >= 500000):
+    harga = math.floor(a * 0.2)
+    print('Anda mendapatkan diskon sebanyak 20% dan harga sekarang adalah Rp.', harga)
+elif (a >= 300000):
+    harga = math.floor(a * 0.1)
+    print('Anda mendapatkan diskon sebanyak 10% dan harga sekarang adalah Rp.', harga)
+elif (a >= 150000):
+    harga = math.floor(a * 0.05)
+    print('Anda mendapatkan diskon sebanyak 5% dan harga sekarang adalah Rp.', harga)
 else:
     harga = print("Anda tidak mendapatkan diskon: ")
-
-print(harga)
